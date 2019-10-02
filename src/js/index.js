ba466de3caac17a44bd5b86cc05244a1d5368236
Osnielky <osnielkisrq@gmail.com>
2019-10-02 02:13:23 +0000

    first try

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,37 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  // console.log(chanche(misc, misc.length));
+
+  var top = document.getElementById("top");
+  var middel = document.getElementById("midle");
+  var botton = document.getElementById("botton");
+  var test = chanche(misc, misc.length);
+
+  top.innerHTML = test;
+  botton.innerHTML = test;
+  middel.innerHTML = chanche(centerNumber, centerNumber.length);
+
+  var child = document.getElementById("p1");
 };
+
+var misc = ["♠", "♣", "♥", "♦"];
+var centerNumber = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+function chanche(array, legua) {
+  var este = Math.floor(Math.random() * legua);
+  return array[este];
+}
